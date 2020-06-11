24d9dfa3325f2b07f7be34de6f001f8ecc23fc49
李亚星 <574633867@qq.com>
2020-06-11 10:53:03 +0800

    test

---
+++ tests/UpploadFileTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *  @department : Commercial development.
+ *  @description : This file is part of [BWMS].
+ *  KKGUAN all rights reserved.
+ */
+require_once '../vendor/autoload.php';
+
+use Xingyalee\UploadFile;
+
+$upload = new UploadFile();
+$upload->maxSize = 1 * 1024 * 1024;    // 默认为-1，不限制上传大小
+$upload->savePath = './upload/';    // 上传根目录
+$upload->saveRule = 'uniqid';       // 上传文件的文件名保存规则
+$upload->uploadReplace = true;           // 如果存在同名文件是否进行覆盖
+$upload->autoSub = true;           // 上传子目录开启
+$upload->subType = 'date';         // 上传子目录命名规则
+$upload->allowExts = ['jpg', 'png']; // 允许类型
+var_dump($upload);
+if ($upload->upload()) {
+    var_dump($upload->getUploadFileInfo());
+} else {
+    var_dump($upload->getErrorMsg());
+}
